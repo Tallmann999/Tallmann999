86baae4f2f0e891c5d2d6b9a5f0db7d45740af99
DESKTOP-MJC0N52\Tallmann <tm_gang@inbox.ru>
2022-01-10 00:42:46 +0300

    камера следит за игроком

---
+++ Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour
+{
+    public float dumping = 1.5f; // небольшое сглаживание камеры (Скольжение)
+     public Vector3 target; // направление камеры
+   
+
+    public GameObject  player;
+
+
+
+    private void Start()
+    {
+        target = transform.position - player.transform.position;
+    }
+    private void Update()
+    {
+        transform.position = player.transform.position + target; 
+    }
+}
