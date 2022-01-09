e6cd39f5bc5b75c8fdbe7ad8865764b152f31eb7
DESKTOP-MJC0N52\Tallmann <tm_gang@inbox.ru>
2022-01-09 21:46:51 +0300

    Анимация персонажа)) кроме прыжка

--- Assets/Scripts/CharacterControllers.cs
+++ Assets/Scripts/CharacterControllers.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class CharacterControllers : MonoBehaviour
 {
-    [SerializeField] private float _speedMove;
-     private float _gravitiPlayer;
+     public float _speedMove;// скорость движения 
+     public float _jumpForse; //сила прыжка
+     private float _gravitiForce; // гравитация персонажа
 
 
-    private Vector3 _moveVector; // направление персонажа 
+    private Vector3 _moveVector; // направление  движения персонажа 
 
-    private CharacterController _chContoller; // ссылка на компонент
-    private Animator _chAnimator;
+    private CharacterController _chContoller; // компонент контроллер
+    private Animator _chAnimator; // компонент аниматор
 
 
     private void Start()
@@ -25,32 +26,40 @@ public class CharacterControllers : MonoBehaviour
         CharacterMove();
         GamingGravity();
     }
-     private void CharacterMove() // метод  перемещения персонажа
+    private void CharacterMove() // метод  перемещения персонажа
     {
+        if (_chContoller.isGrounded) // ограничение движения во время прыжка
+        {
+            // Перемещение по поверхности
+            _moveVector = Vector3.zero; // обновление вектора движения
+            _moveVector.x = Input.GetAxis("Horizontal") * _speedMove ; //оси движения персонажа
+            _moveVector.z = Input.GetAxis("Vertical") * _speedMove ;// оси движения персонажа
 
-        // Перемещение по поверхности
-        _moveVector = Vector3.zero;
-        _moveVector.x = Input.GetAxis("Horizontal")* _speedMove;
-        _moveVector.z = Input.GetAxis("Vertical") * _speedMove;
+
+            // анимация передвижения персонажа
+            if (_moveVector.x != 0 || _moveVector.z != 0) _chAnimator.SetBool("run", true);//условие при котором будет меняться анимация
+            else _chAnimator.SetBool("run", false);
 
 
-        // Поворот персонажа в сторону направления перемещения
-        if(Vector3.Angle(Vector3.forward,_moveVector) >1f || Vector3.Angle(Vector3.forward, _moveVector) == 0)
+            // Поворот персонажа в сторону направления перемещения
+            if (Vector3.Angle(Vector3.forward, _moveVector) > 1f || Vector3.Angle(Vector3.forward, _moveVector) == 0)
             {
 
-            Vector3 direct = Vector3.RotateTowards(transform.forward, _moveVector, _speedMove, 0.0f);
-            transform.rotation = Quaternion.LookRotation(direct);
+                Vector3 direct = Vector3.RotateTowards(transform.forward, _moveVector, _speedMove, 0.0f);
+                transform.rotation = Quaternion.LookRotation(direct);// поворот вектрора
+            }
         }
-
-        _moveVector.y = _gravitiPlayer;// расчёт гравитации выполнять после поворота!!!
-        _chContoller.Move(_moveVector * Time.deltaTime); //метод передвижения по направлению
+            _moveVector.y = _gravitiForce;// расчёт гравитации выполнять после поворота!!!
+            _chContoller.Move(_moveVector* Time.deltaTime) ; //метод передвижения по направлению
+        
     }
 
 
-    private void GamingGravity()
+    private void GamingGravity() // гравитация персонажа
     {
-        if (!_chContoller.isGrounded) _gravitiPlayer -= 20f * Time.deltaTime;
-        else _gravitiPlayer = -1f;
+        if (!_chContoller.isGrounded) _gravitiForce -= 20f ;// проверка персонажа на гравитацию
+        else _gravitiForce = -1f; //после приземления сила гравитации -1
+        if (Input.GetKeyDown(KeyCode.Space) && _chContoller.isGrounded) _gravitiForce = _jumpForse; // прыжок персонажа
 
     }
 }
